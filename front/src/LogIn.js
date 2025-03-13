63811d1eeafa15d2b9f0c19db638db2187dd1be7
VictorManuelAlvarezMayorga <utm23090718@utma.edu.mx>
2025-03-13 12:13:17 -0600

    correcciones index y log in

--- front/src/LogIn.js
+++ front/src/LogIn.js
@@ -1,5 +1,36 @@
 import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
-
+import React, { useState } from "react";
+ import { useNavigate } from "react-router-dom";
+ import axios from "axios";
+ 
+ const App = () => {
+   const [data, setData] = useState({});
+   const navigate = useNavigate();
+ 
+   const onChangeLogin = (e) => {
+     e.preventDefault();
+     const updatedData = { ...data };
+     updatedData[e.target.name] = e.target.value;
+     setData(updatedData);
+     console.log(updatedData);
+   };
+ 
+   const onSubmitData = async (e) => {
+     e.preventDefault();
+     // Verifica que los datos no esten incompletos
+ 
+     try {
+       const res = await axios.post("http://localhost:4000/users/sign-in", data);
+       const user = res.data.user;
+       user.logined = true;
+       localStorage.user = JSON.stringify(user);
+       navigate("/inicio");
+     } catch (error) {
+       alert("Algo salió mal", error);
+       console.log(error)
+     }
+   };
+ 
   return (
     <Container className="mt-3">
       <Card className="mb-5" style={{ width: "30rem", margin: "auto" }}>
@@ -39,5 +70,6 @@ import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
     </Container>
   );
 
+ };
 
 export default App;
